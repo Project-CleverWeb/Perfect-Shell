d48d0bc2f603f4ef5c4ffdbc706b8dc7170ac2fc
Nicholas Jordon <nick@empowernetwork.com>
2013-01-23 10:28:00 -0500

    made some head-way on the windows installer.

--- windows/install.bat
+++ windows/install.bat
@@ -1,6 +1,16 @@
 @echo off
 
-rem register values here
+rem Perfect-Shell vars
+set pshell_ver="1.0"
+set pshell_keyword="pshell"
+set pshell_errors="none"
+
+
+rem get the OS architecture
+set os_arc="64"
+if not exist "%systemdrive%\Program Files (x86)" set os_arc="32"
+
+rem check for admin rights here
 rem check for git here
 
 cls
@@ -12,18 +22,53 @@ echo                              Welcome to Perfect-Shell
 echo.
 echo                      Version: 1.0
 echo                      Requirements:
-echo                      Errors: (if any)
+echo                      Errors: %pshell_errors%
 echo.
 echo            \--------------------------------------------------------/
 echo.
-echo Start? (y/n/c)
+echo.
+CHOICE /C YN /M Do you wish to start installation? Yes (y), No (n) 
+IF errorlevel 2 goto fail
+IF errorlevel 1 goto start
 
+
+:fail
+cls
+echo.
+echo.
+echo            /--------------------------------------------------------\
+echo.
+echo                               Installation Aborted!
+echo.
+echo                      Errors: %pshell_errors%
+echo.
+echo            \--------------------------------------------------------/
+echo.
+echo.
+echo Exiting...
+@ping 1.1.1.1 -n 1 -w 5000 > nul
+exit
+
+:start
 rem ask for custom keyword here
-rem ask for OS version here (32/64)
+echo Warning: Only set this if you know what you are doing!
+CHOICE /C YN /M Do you wish to set your own start command? Yes (y), No (n) 
+IF errorlevel 2 goto install
+IF errorlevel 1 goto set_keyword
 
+:set_keyword
+echo What command would you like to type to start Perfect-Shell?
+echo Keep in mind, this cannot be an existing command.
+set /p pshell_keyword="What command would you like to type to start Perfect-Shell?"
+
+
+:install
+CHOICE /C YN /M Last Chance: Are you sure you wish to install? Yes (y), No (n)
+IF errorlevel 2 goto fail
 rem (install)
 
-
+:success
+cls
 echo.
 echo.
 echo            /--------------------------------------------------------\
@@ -31,11 +76,15 @@ echo.
 echo                      Thank you for installing Perfect-Shell!
 echo.
 echo                      Activation Keyword: pshell (default)
-echo                      Errors: (if any)
+echo                      Install Path: meh/pshell.bat
+echo                      Errors: %pshell_errors%
 echo.
 echo            \--------------------------------------------------------/
 echo.
-
+echo.
+echo Press any key to exit
+pause>nul
+exit
 
 rem default length of cmd window 
 rem  --------------------------------------------------------------------------------
